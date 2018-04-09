1beecbb2ebedd697e56fbec9e3e75ece47437e2c
andrewthenintendrone <andrewfitzpatrick@y7mail.com>
2018-04-09 11:28:21 +1000

    Very early Level Rotate Script

--- Assets/Scripts/RotateLevel.cs
+++ Assets/Scripts/RotateLevel.cs
@@ -2,15 +2,38 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class RotateLevel : MonoBehaviour {
+public class RotateLevel : MonoBehaviour
+{
+    public bool rotating = false;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
+    public float degreesToRotate = 90.0f;
+    public float lastAngle = 0.0f;
+    public float currentAngle = 0.0f;
 	
-	// Update is called once per frame
-	void Update () {
-		
+	void Update ()
+    {
+		if(Input.GetKeyDown(KeyCode.Space) && !rotating)
+        {
+            rotating = true;
+        }
+
+        if(rotating)
+        {
+            currentAngle += degreesToRotate * Time.deltaTime;
+
+            if(currentAngle >= (lastAngle + degreesToRotate))
+            {
+                currentAngle = lastAngle + degreesToRotate;
+                rotating = false;
+                lastAngle += degreesToRotate;
+                if(lastAngle == 270)
+                {
+                    currentAngle = -90;
+                    lastAngle = -90;
+                }
+            }
+
+            transform.eulerAngles = new Vector3(0, 0, currentAngle);
+        }
 	}
 }
