1aefb5464f6f7a514e27f97d7ff2eaf70edf6151
andrewthenintendrone <andrewfitzpatrick@y7mail.com>
2018-04-09 14:12:46 +1000

    more sprite stuff

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -2,50 +2,96 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[System.Serializable]
+public class PlayerSettings
+{
+    public float moveSpeed = 1000;
+    public float jumpForce = 100;
+    public float health = 5;
+}
+
 [RequireComponent(typeof(SpriteRenderer))]
 [RequireComponent(typeof(Collider2D))]
 [RequireComponent(typeof(Rigidbody2D))]
 [RequireComponent(typeof(Animator))]
 public class Player : MonoBehaviour
 {
-    [System.Serializable]
-    public struct PlayerSettings
-    {
-        public float moveSpeed;
-        public float jumpForce;
-        public float health;
-    }
-
+    // player settings
     public PlayerSettings playerSettings;
-    private Rigidbody2D rb;
+
+    // component references
+    SpriteRenderer spriteRenderer;
+    Collider2D myCollider;
+    Rigidbody2D rb;
+    Animator animator;
+
+    // raycast2d hit info
     private RaycastHit2D hitInfo;
+
+    // current player movement
+    private Vector2 movement;
+
+    // is the player walking
+    bool walking = false;
+    // is the player jumping
+    bool jumping = false;
+    // is te player pushing
+    bool pushing = false;
 
 	void Start ()
     {
+        // get references to components
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        myCollider = GetComponent<Collider2D>();
         rb = GetComponent<Rigidbody2D>();
-	}
+        animator = GetComponent<Animator>();
+    }
 	
 	void Update ()
     {
+        move();
+        checkState();
+	}
+
+    // move the player
+    void move()
+    {
+        // get inputs
         float horizontal = Input.GetAxisRaw("Horizontal");
         float vertical = Input.GetAxisRaw("Vertical");
 
-        if(horizontal < 0)
+        // update movement
+        movement = new Vector2(horizontal * playerSettings.moveSpeed * Time.deltaTime, 0);
+
+        // apply movement to rigidbody
+        rb.AddForce(movement, ForceMode2D.Force);
+    }
+
+    // check the state of the player
+    void checkState()
+    {
+        // flip sprite if neccesary
+        if(movement.x != 0)
         {
-            GetComponent<SpriteRenderer>().flipX = true;
-        }
-        else if (horizontal > 0)
-        {
-            GetComponent<SpriteRenderer>().flipX = false;
+            spriteRenderer.flipX = movement.x < 0;
         }
 
+        // check if the player is walking
+        walking = (movement.x != 0);
+
+        // check if the player is standing on something
         hitInfo = Physics2D.Raycast(transform.position, Vector2.down, 0.25f);
-        Debug.DrawLine(transform.position, transform.position + Vector3.down * 0.25f, Color.red);
+        Debug.DrawLine(transform.position, transform.position + Vector3.down * 0.25f, Color.green);
+        jumping = (hitInfo.collider == null);
 
-        GetComponent<Animator>().SetBool("jumping", hitInfo.collider == null);
+        // check if the player is pushing a block
+        hitInfo = Physics2D.Raycast(transform.position, Vector2.right * Mathf.Sign(movement.x), 0.25f);
+        Debug.DrawLine(transform.position, transform.position + Vector3.right * 0.25f, Color.red);
+        pushing = hitInfo.collider != null;
 
-        GetComponent<Animator>().SetBool("walking", (rb.velocity.x != 0));
-
-        rb.AddForce(new Vector2(horizontal * playerSettings.moveSpeed * Time.deltaTime, 0), ForceMode2D.Force);
-	}
+        // update sprite animations
+        GetComponent<Animator>().SetBool("jumping", jumping);
+        GetComponent<Animator>().SetBool("walking", walking);
+        GetComponent<Animator>().SetBool("pushing", pushing);
+    }
 }
