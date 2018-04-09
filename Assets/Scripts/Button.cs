a9f26b6d58c6e745ba8e8e9c9a8077cd1fbd1317
Aiden <a.sharp3197@gmail.com>
2018-04-09 16:16:17 +1000

    Pressure Plate and door

---
+++ Assets/Scripts/Button.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Button : MonoBehaviour {
+
+    public GameObject door;
+	// Use this for initialization
+	void Start ()
+    {
+        door.GetComponent<Animator>().StopPlayback();
+	}
+
+    private void Update()
+    {
+        
+    }
+    // Update is called once per frame
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+     
+        if (collision.GetComponent<Rigidbody2D>() != null)
+        {
+
+
+            if (collision.GetComponent<Rigidbody2D>().mass >= 3)
+            {
+               // transform.localScale -= new Vector3(0, 0.1f, 0) * Time.deltaTime;
+                // transform.position -= new Vector3(0, 0.1f, 0) * Time.deltaTime;
+                door.GetComponent<Animator>().SetBool("isOpen",true);
+
+                //Debug.Log(door.GetComponent<Animator>().speed);
+            }
+        }
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        door.GetComponent<Animator>().SetBool("isOpen", false);
+    }
+}
