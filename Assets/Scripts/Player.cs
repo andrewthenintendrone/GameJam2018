6c43f7fcafd26bc07c688ada474a6646d03a5757
andrewthenintendrone <andrewfitzpatrick@y7mail.com>
2018-04-09 12:01:37 +1000

    player rotation mostly works

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Sprite))]
+[RequireComponent(typeof(Collider2D))]
+[RequireComponent(typeof(Rigidbody2D))]
+public class Player : MonoBehaviour
+{
+    [System.Serializable]
+    public struct PlayerSettings
+    {
+        public float moveSpeed;
+        public float jumpForce;
+        public float health;
+    }
+
+    public PlayerSettings playerSettings;
+    private Rigidbody2D rb;
+
+	void Start ()
+    {
+        rb = GetComponent<Rigidbody2D>();
+	}
+	
+	void Update ()
+    {
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
+
+        rb.AddForce(new Vector2(horizontal * playerSettings.moveSpeed * Time.deltaTime, 0), ForceMode2D.Force);
+	}
+}
--- Assets/Scripts/RotateLevel.cs
+++ Assets/Scripts/RotateLevel.cs
@@ -9,12 +9,29 @@ public class RotateLevel : MonoBehaviour
     public float degreesToRotate = 90.0f;
     public float lastAngle = 0.0f;
     public float currentAngle = 0.0f;
-	
-	void Update ()
+
+    public List<GameObject> levelObjects;
+    private GameObject player;
+
+    private void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    void Update ()
     {
 		if(Input.GetKeyDown(KeyCode.Space) && !rotating)
         {
             rotating = true;
+
+            // freeze level objects
+            foreach(GameObject currentObject in levelObjects)
+            {
+                currentObject.GetComponent<Rigidbody2D>().simulated = false;
+                currentObject.transform.parent = transform;
+            }
+            player.GetComponent<Rigidbody2D>().simulated = false;
+            player.transform.parent = transform;
         }
 
         if(rotating)
@@ -31,8 +48,21 @@ public class RotateLevel : MonoBehaviour
                     currentAngle = -90;
                     lastAngle = -90;
                 }
+
+                // unfreeze level objects
+                foreach (GameObject currentObject in levelObjects)
+                {
+                    currentObject.GetComponent<Rigidbody2D>().simulated = true;
+                    currentObject.transform.parent = null;
+                }
+                player.GetComponent<Rigidbody2D>().simulated = true;
+                player.transform.parent = null;
             }
 
+            if(rotating)
+            {
+                player.transform.localEulerAngles = new Vector3(0, 0, -currentAngle);
+            }
             transform.eulerAngles = new Vector3(0, 0, currentAngle);
         }
 	}
