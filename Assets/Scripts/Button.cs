0663bcb9a8cb50c0b2482aaa46a0a6a7a4a72c13
andrewthenintendrone <andrewfitzpatrick@y7mail.com>
2018-04-10 16:46:30 +1000

    multi button door level
    - every level works
    - more graphics again

--- Assets/Scripts/Button.cs
+++ Assets/Scripts/Button.cs
@@ -2,11 +2,15 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Button : MonoBehaviour {    
-    public GameObject door;	
+public class Button : MonoBehaviour
+{    
+    public GameObject door;
+    public List<GameObject> otherButtons;
+    private bool pressed = false;
+
+
 	void Start ()
     {
-        
         //default the door to not play
         door.GetComponent<Animator>().StopPlayback();
 	}
@@ -14,16 +18,30 @@ public class Button : MonoBehaviour {
    //When an object hits the button open the door
     private void OnTriggerEnter2D(Collider2D collision)
     {
-     
         if (collision.GetComponent<Rigidbody2D>() != null)
-        {          
-                door.GetComponent<Animator>().SetBool("isOpen",true);           
+        {
+            pressed = true;
+        }
+
+        bool allPreseed = true;
+        foreach(GameObject currentButton in otherButtons)
+        {
+            if(!currentButton.GetComponent<Button>().pressed)
+            {
+                allPreseed = false;
+            }
+        }
+
+        if(allPreseed)
+        {
+            door.GetComponent<Animator>().SetBool("isOpen", true);
         }
     }
 
     //when there is no longer an object on the button close the door
     private void OnTriggerExit2D(Collider2D collision)
     {
+        pressed = false;
         door.GetComponent<Animator>().SetBool("isOpen", false);
     }
 }
