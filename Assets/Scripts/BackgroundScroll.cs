9a0d6480e480379bba81df302eb36ea77ef17bfc
andrewthenintendrone <andrewfitzpatrick@y7mail.com>
2018-04-10 11:41:49 +1000

    updated graphics
    - background scrolling script

---
+++ Assets/Scripts/BackgroundScroll.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundScroll : MonoBehaviour
+{
+    public float startPositionX;
+    public float loopPositionX;
+    public float scrollSpeedX;
+
+	void Update ()
+    {
+        transform.Translate(Vector3.right * scrollSpeedX * Time.deltaTime);
+
+        if(transform.position.x <= loopPositionX)
+        {
+            transform.position = Vector3.right * startPositionX;
+        }
+	}
+}
