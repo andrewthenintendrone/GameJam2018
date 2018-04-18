1adb1738d83a69159d76bc79c3a029c03a0fae10
Andrew Fitzpatrick <andrewfitzpatrick@y7mail.com>
2018-04-18 11:41:45 +1000

    Sticky block
    - Sticky block freezes another object for a set amount of time
    - 2nd new level greyboxed (not tested)
    - removed old scripts

---
+++ Assets/Scripts/StickyBlock.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(BoxCollider2D))]
+public class StickyBlock : MonoBehaviour
+{
+    [Tooltip("how long blocks should stick on for")]
+    public float stickTime = 3.0f;
+
+    private GameObject stuckObject = null;
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        // don't freeze the player
+        if (stuckObject == null && other.tag != "Player")
+        {
+            stuckObject = other.gameObject;
+            stuckObject.GetComponent<Rigidbody2D>().constraints = RigidbodyConstraints2D.FreezePosition;
+            Invoke("releaseObject", stickTime);
+        }
+    }
+
+    // releases the object
+    void releaseObject()
+    {
+        stuckObject.GetComponent<Rigidbody2D>().constraints = RigidbodyConstraints2D.None;
+        stuckObject = null;
+    }
+}
