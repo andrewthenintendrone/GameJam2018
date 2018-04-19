fcc13672a5163477dd02a877671fdcde0225dcdd
Andrew Fitzpatrick <andrewfitzpatrick@y7mail.com>
2018-04-19 10:58:06 +1000

    Dummy title screen and fixed instructions

---
+++ Assets/Scripts/Title.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+public class Title : MonoBehaviour
+{
+
+	public void Play()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void Exit()
+    {
+        #if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+        #endif
+        Application.Quit();
+    }
+}
